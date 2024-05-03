0c26286154514aee626e60115747b5d9d611e87f
Kevin Backup <kvchang@umich.edu>
2024-05-03 17:36:45 -0400

    dark and light

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,17 @@
+
 function darkMode() {
     var element = document.body;
+    
     element.classList.toggle("dark-mode");
-    if(document.getElementsByClassName("darkModeButton")[0].textContent == "🌙"){
+    if(document.getElementsByClassName("darkModeButton")[0].textContent == "🌑"){
         document.getElementsByClassName("darkModeButton")[0].textContent = "☀️";
+        document.getElementsByClassName("darkModeButton")[0].style.backgroundColor = "#3A3858";
+        document.getElementById("navbar").style.backgroundColor="#352B42";
+
     }else{
-        document.getElementsByClassName("darkModeButton")[0].textContent = "🌙";
+        document.getElementsByClassName("darkModeButton")[0].textContent = "🌑";     
+        document.getElementById("navbar").style.backgroundColor="#E5CEB4";
+        document.getElementsByClassName("darkModeButton")[0].style.backgroundColor = "#F2F0E5";
     }
+    
 }
