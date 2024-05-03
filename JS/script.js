6f488b7c2061cb3c2f10330ccc2a421e446b85f7
Kevin Backup <kvchang@umich.edu>
2024-05-02 22:51:00 -0400

    fix

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,9 @@
 function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
+    if(document.getElementsByClassName("darkModeButton")[0].textContent == "🌙"){
+        document.getElementsByClassName("darkModeButton")[0].textContent = "☀️";
+    }else{
+        document.getElementsByClassName("darkModeButton")[0].textContent = "🌙";
+    }
 }
